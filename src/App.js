ea2b0213bd4296c444b221473b4ffa839a4f3f1e
J-ON-AS <118046576+J-ON-AS@users.noreply.github.com>
2023-10-21 12:03:53 +0530

    Heavy Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/textform';
+import  React, {useState} from 'react';
 function App() {
+  const [mode,setMode] = useState('light');
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+    setMode('dark');
+    document.body.style.color = 'white';
+    document.body.style.backgroundColor = '#500b58';
+  }else{
+    setMode('light');
+    document.body.style.color = 'black';
+    document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-    <Navbar title='Cristiano Ronaldo' about='About us' />
+    <Navbar title='Cristiano Ronaldo' about='About us'  mode={mode} toggleMode={toggleMode}/>
     <div className="container">
-    <Textform />
+    <Textform  mode={mode}/>
     </div>
     
     </>
--- src/components/textform.js
+++ src/components/textform.js
@@ -2,27 +2,27 @@ import React ,{useState} from 'react'
 
 export default function Textform(props) {
 
-  const [myStyle , setMyStyle] = useState({
-    color: 'white',
-    backgroundColor : 'black'
-  })
+  // const [myStyle , setMyStyle] = useState({
+  //   color: 'white',
+  //   backgroundColor : 'black'
+  // })
   const [text , setText] = useState('Enter Text Here')
-  const [btnText, setBtnText] = useState("Enable Dark Mode")
+  // const [btnText, setBtnText] = useState("Enable Light Mode")
 
-  const toggleStyle = () => {
-    if(myStyle.color ==='white'){
-    setMyStyle({
-      color: 'black',
-      backgroundColor : 'white'
-    })
-    setBtnText("Enable  Dark Mode");
-  }else if(myStyle.color ==='black'){
-    setMyStyle({
-      color: 'white',
-      backgroundColor : 'black'
-    })
-    setBtnText("Enable Light Mode");
-  }}
+  // const toggleStyle = () => {
+  //   if(myStyle.color ==='white'){
+  //   setMyStyle({
+  //     color: 'black',
+  //     backgroundColor : 'white'
+  //   })
+  //   setBtnText("Enable  Dark Mode");
+  // }else if(myStyle.color ==='black'){
+  //   setMyStyle({
+  //     color: 'white',
+  //     backgroundColor : 'black'
+  //   })
+  //   setBtnText("Enable Light Mode");
+  // }}
   const handleUpClick = () =>{
     let newText = text.toUpperCase();
     setText(newText);
@@ -34,21 +34,35 @@ export default function Textform(props) {
   const handleOnChange = (event) =>{
     setText(event.target.value)
   };
+  const handleExtraSpace = () =>{
+    let newText = text.split(/[ ]+/) ;
+    setText(newText.join(" "));
+  };
+  const handleClearSpace = () =>{
+    let newText = "";
+    setText(newText);
+  };
+  const handleCopy = () =>{
+    let element = document.querySelector('#myBox');
+    element.select();
+    navigator.clipboard.writeText(element.value);
+  };
   return (
-    < >
-    <div style={myStyle} className='p-5 m-2' >
+    <>
+    <div  className='m-2' >
       <h1>{props.heading}</h1>
-      <div  style={myStyle} className="mb-3">
-      <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="7"></textarea>
+      <div   className="mb-3">
+      <textarea style={{backgroundColor:props.mode==='dark'?'#6b2673':'white', color:props.mode==='dark'?'white':'black'}} className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="7"></textarea>
       </div>
-      <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
-      <button className="btn btn-primary" onClick={handleDownClick}>Convert to LowerCase</button>
+      <button className={`btn btn-${props.mode==='dark'?'info':'primary'} mx-2 my-2`} onClick={handleUpClick}>Convert to UpperCase</button>
+      <button className={`btn btn-${props.mode==='dark'?'info':'primary'} mx-2 my-2`} onClick={handleDownClick}>Convert to LowerCase</button>
+      {/* <button onClick={toggleStyle} type='button' className={`btn btn-${props.mode==='dark'?'info':'primary'} mx-2 my-2`}>{btnText}</button> */}
+      <button onClick={handleCopy} type='button' className={`btn btn-${props.mode==='dark'?'info':'primary'} mx-2 my-2`}>Copy Text</button>
+      <button onClick={handleExtraSpace} type='button' className={`btn btn-${props.mode==='dark'?'info':'primary'} mx-2 my-2`}>Remove Extra Space</button>
+      <button onClick={handleClearSpace} type='button' className={`btn btn-${props.mode==='dark'?'info':'primary'} mx-2 my-2`}>Clear Text</button>
     </div>
-      <p class='mx-2 my-1'>Number of letters in paragraph : {text.length}  </p>
-      <p class='mx-2 my-1'>Number of words in paragraph : {text.split(' ').length} </p>
-
-      <button onClick={toggleStyle} type='button' className='btn btn-primary'>{btnText}</button>
-  
-    </>
+      <p className='mx-2 my-1'>Number of letters in paragraph : {text.length}  </p>
+      <p className='mx-2 my-1'>Number of words in paragraph : {text.split(' ').length} </p>
+    </>  
   )
   }
